31f2a4428fdcf59ac79cec44b74e93e0d681b645
droidifi <admin@droidifi.com>
2020-12-04 11:58:38 -0500

    merge NXP imx_5.4.47_2.2.0

--- drivers/clk/imx/clk-imx8mm.c
+++ drivers/clk/imx/clk-imx8mm.c
@@ -367,7 +367,7 @@ static const char *imx8mm_clkout_sels[] = { "audio_pll1", "audio_pll2", "video_p
 static struct clk *clks[IMX8MM_CLK_END];
 static struct clk_onecell_data clk_data;
 
-static int __init imx_clk_init_on(struct device_node *np,
+static int imx_clk_init_on(struct device_node *np,
 				  struct clk * const clks[])
 {
 	u32 *array;
@@ -727,7 +727,11 @@ static struct platform_driver imx8mm_clk_driver = {
 	},
 };
 module_platform_driver(imx8mm_clk_driver);
+MODULE_AUTHOR("Bai Ping <ping.bai@nxp.com>");
+MODULE_DESCRIPTION("NXP i.MX8MM clock driver");
+MODULE_LICENSE("GPL v2");
 
+#ifndef MODULE
 /*
  * Debugfs interface for audio PLL K divider change dynamically.
  * Monitor control for the Audio PLL K-Divider
@@ -805,3 +809,4 @@ static int __init pll_debug_init(void)
 }
 late_initcall(pll_debug_init);
 #endif /* CONFIG_DEBUG_FS */
+#endif /* MODULE */
